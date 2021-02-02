6447590f95ba7518e1990dacffd8a803ff91d5b4
xXGoziXx <nosarobo@gmail.com>
2021-02-02 04:24:10 +0000

    got rid off redundant files

--- client/src/app/pipe/ngfuse.pipe.spec.ts
+++
@@ -1,9 +0,0 @@
-import { NgFusePipe } from './ngfuse.pipe';
-
-import { NgFuseService } from '../services/ngfuse.service';
-describe('NgFusePipe', () => {
-  it('create an instance', () => {
-    const pipe = new NgFusePipe(new NgFuseService);
-    expect(pipe).toBeTruthy();
-  });
-});
